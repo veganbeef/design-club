82b85e5b968b7dbf572445a2a7d1f2f53b3f2c93
veganbeef <veganbeef@protonmail.com>
2025-05-02 10:12:24 -0400

    update error

--- app/components/Designs.tsx
+++ app/components/Designs.tsx
@@ -172,7 +172,7 @@ export function Designs({ designInfoArray }: TabProps) {
       } catch (error) {
         console.error("Failed to vote:", error);
         const errorString = error instanceof Error ? error.message : String(error);
-        setError("Failed to vote. ".concat(errorString));
+        setError(`Failed to vote. Signer: ${signer} Error: ${errorString}`);
       }
     },
     [signer, address]
