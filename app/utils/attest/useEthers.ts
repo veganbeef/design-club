8a29944b67aba764fdae65bff1c804702a1d8920
Quazia <arthur@neynar.com>
2025-05-02 10:47:38 -0400

    chore: all the logs

--- app/utils/attest/useEthers.ts
+++ app/utils/attest/useEthers.ts
@@ -5,13 +5,19 @@ import { type Config, useConnectorClient } from 'wagmi'
 
 export function clientToSigner(client: Client<Transport, Chain, Account>) {
   const { account, chain, transport } = client
+  console.log('clientToSigner', client)
+  console.log('account', account)
+  console.log('chain', chain)
+  console.log('transport', transport)
   const network = {
     chainId: chain.id,
     name: chain.name,
     ensAddress: chain.contracts?.ensRegistry?.address,
   }
   const provider = new BrowserProvider(transport, network)
+  console.log('provider', provider)
   const signer = new JsonRpcSigner(provider, account.address)
+  console.log('signer', signer)
   return signer
 }
 
