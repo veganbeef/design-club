002d9b598980a468f2b05e00d8f0addf832a4055
veganbeef <veganbeef@protonmail.com>
2025-05-02 11:35:04 -0400

    fix build

--- app/utils/attest/useEthers.ts
+++ app/utils/attest/useEthers.ts
@@ -1,5 +1,4 @@
 import { BrowserProvider, JsonRpcSigner } from 'ethers'
-import { useMemo } from 'react'
 import type { Account, Chain, Client, Transport } from 'viem'
 import { type Config, useConnectorClient } from 'wagmi'
 
